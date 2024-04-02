cf6000544766b1f2d57a5c5826569944aa731f7b
Reign-Beaux <samc920130@hotmail.com>
2024-04-02 01:02:49 -0600

    Salto de pared

--- player.gd
+++ player.gd
@@ -6,11 +6,13 @@ extends CharacterBody2D
 @onready var coyote_jump_timer: Timer = $CoyoteJumpTimer
 
 var air_jump = false
+var just_wall_jumped = false
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
 	var horizontal_input := Input.get_axis("ui_left", "ui_right")
 	apply_gravity(delta)
+	handle_wall_jump()
 	handle_jump()
 	handle_acceleration(horizontal_input, delta)
 	handle_air_acceleration(horizontal_input, delta)
@@ -22,18 +24,35 @@ func _physics_process(delta: float) -> void:
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
 	if just_left_ledge:
 		coyote_jump_timer.start()
+	just_wall_jumped = false
 
 func apply_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * movement_data.GRAVITY_SCALE * delta
 
+func handle_wall_jump():
+	if not is_on_wall_only():
+		return
+	
+	var wall_normal = get_wall_normal()
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.x = wall_normal.x * movement_data.SPEED
+		velocity.y = movement_data.JUMP_VELOCITY
+		just_wall_jumped = true
+
 func handle_jump() -> void:
+	if is_on_floor():
+		air_jump = true
+	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if  Input.is_action_just_pressed("ui_accept"):
 			velocity.y = movement_data.JUMP_VELOCITY
 	if not is_on_floor():
 		if Input.is_action_just_released("ui_accept") and velocity.y < movement_data.JUMP_VELOCITY / 2:
 			velocity.y = movement_data.JUMP_VELOCITY / 2
+		if Input.is_action_just_pressed("ui_accept") and air_jump and not just_wall_jumped:
+			velocity.y = movement_data.JUMP_VELOCITY * 0.8
+			air_jump = false
 
 func handle_acceleration(horizontal_input: float, delta: float):
 	if not is_on_floor():
